cf6c281a732b78dddc716d9a3ff91a7c843f235f
Thomas Kingsley <kingsleythomas33@gmail.com>
2021-11-24 18:18:59 +0100

    mise en place  des if connexion deconnexion vendre annonce pour user

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -12,7 +12,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class HomeController extends AbstractController
 {
     /**
-     * @Route("/home", name="home")
+     * @Route("/", name="home")
      */
     public function index(CarRepository $repository, Request $request): Response
     {
