51d33f86217fbad33ee8632cc2c9d6cf2a2634b5
Cyrdard Rugamba <rcyrdard@gmail.com>
2021-11-25 14:34:27 +0100

    script de validation form

--- public/js/add.js
+++ public/js/add.js
@@ -1,40 +1,23 @@
 
-
 let listInputs = document.querySelectorAll('input');
-listInputs.style = 'color:red';
-
-
 let btn = document.getElementById('sub');
 let year = document.getElementById('form_year');
 
 
 
-// let anno = document.getElementById("conteneur");
-
-// if (!anno) {
-//     console.log('rien');
-// }
-
-// console.log(document);
-// console.log(document.querySelector('body'));
-
-
-
-
-
 // document.querySelector("form").addEventListener("submit", formValid)
+// document.formulaire.addEventListener('submit', formValid);
 
 // console.log(document.querySelector("form"));
-
 
 btn.addEventListener('click', formValid);
 
 function formValid() {
+
     // e.preventDefault();
     for (input of listInputs) {
 
         let p1 = document.createElement('p');
-        // p1.classList.add = 'error'
         p1.style.color = 'red'
         p1.style.fontWeight = 'bold'
         input.parentElement.append(p1)
@@ -43,19 +26,14 @@ function formValid() {
         // inputElement.parentNode.lastElementChild.innerText = "";
 
         if ((input.value) == "") {
-            // e.preventDefault();
             p1.innerHTML = 'Veuillez remplire le champs vide';
+
         }
-        // input = false;
-        // inputElement.parentNode.lastElementChild.innerText = "";
+        console.log(year.value.length);
 
+        if (year.value.length != 4) {
+            year.parentNode.lastChild.innerHTML = 'Veuillez mettre au minimum et au maximum 4 chiffres';
+        }
     }
-
-    if (input.value.length <= 4 || input.value.length >= 4) {
-
-        year.innerHTML = 'Veuillez mettre au minimum et au maximum 4 chiffres';
-
-    }
-
 
 }
