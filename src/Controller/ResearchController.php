da8779816bb5d84569462ef4e47cd3baac94a595
Vincent Roumy <vincent.roumy@colombbus.org>
2021-11-26 10:42:11 +0100

    accéder aux noms du vendeur

--- src/Controller/ResearchController.php
+++ src/Controller/ResearchController.php
@@ -7,6 +7,7 @@ use App\Data\SearchData;
 use App\Form\SearchForm;
 use App\Repository\CarRepository;
 use App\Repository\BookingRepository;
+use App\Repository\UsersRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -24,14 +25,16 @@ class ResearchController extends AbstractController
     /**
      * @Route("/research", name="research")
      */
-    public function index(CarRepository $repository, BookingRepository $booking, Request $request): Response
+    public function index(CarRepository $repository, BookingRepository $booking, UsersRepository $sellersRepo, Request $request): Response
     { 
 
         //Liste des voitures réservées
-        //$bookedCars = $booking->findAllBookings();
         $bookedCars = $booking->findAll();
 
-        //dd($bookedCars);
+        //List des vendeurs
+        $sellers = $sellersRepo->findAll();
+
+        //dd($sellers);
         
         $data = new SearchData();
         $data->page = $request->get('page', 1);
@@ -79,7 +82,7 @@ class ResearchController extends AbstractController
 
         if($request->get('ajax')) {
             return new JsonResponse([
-                'content' => $this->renderView('research/_cars.html.twig', ['cars' => $cars, 'bookedCars' => $bookedCars]),
+                'content' => $this->renderView('research/_cars.html.twig', ['cars' => $cars, 'bookedCars' => $bookedCars, 'sellers' => $sellers]),
                 'sorting' => $this->renderView('research/_sorting.html.twig', ['cars' => $cars]),
                 'pagination' => $this->renderView('research/_pagination.html.twig', ['cars' => $cars]),
                 //'models' => $this->renderView('research/_models.html.twig', ['models' =>  $data->models]),
@@ -96,7 +99,8 @@ class ResearchController extends AbstractController
             'form' => $form->createView(),
             'minPrice' => $minPrice,
             'maxPrice' => $maxPrice,
-            'bookedCars' => $bookedCars
+            'bookedCars' => $bookedCars,
+            'sellers' => $sellers
         ]);
     }
 }
